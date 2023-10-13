d14c0b780f8914c3592cc474e9a89ea6adb38e6a
DevRuby <hiveruby@gmail.com>
2023-10-13 09:21:20 +0900

    feat: Add Test class in MemoryOptimizer.Test project

---
+++ MemoryOptimizer/MemoryOptimizer.Test/Test.cs
@@ -0,0 +1,16 @@
+﻿using MemoryOptimizer.Core;
+
+namespace MemoryOptimizer.Test;
+
+internal class Test
+{
+    private static async Task Main()
+    {
+        string[] processNames = { "firefox.exe" };
+
+        var memoryCleaner = new MemoryCleaner(processNames, 500);
+
+        var cancellationTokenSource = new CancellationTokenSource();
+        await memoryCleaner.RunAsync(cancellationTokenSource.Token);
+    }
+}
