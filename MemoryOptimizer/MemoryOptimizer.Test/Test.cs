026f8dea61f028f45dff45ef921a1e34e70107ed
DevRuby <hiveruby@gmail.com>
2023-10-24 17:26:59 +0900

    test: Update test code

--- MemoryOptimizer/MemoryOptimizer.Test/Test.cs
+++ MemoryOptimizer/MemoryOptimizer.Test/Test.cs
@@ -4,13 +4,18 @@ namespace MemoryOptimizer.Test;
 
 internal class Test
 {
+    
     private static async Task Main()
     {
         string[] processNames = { "firefox.exe" };
 
         var memoryCleaner = new MemoryCleaner(processNames, 500);
 
-        var cancellationTokenSource = new CancellationTokenSource();
-        await memoryCleaner.RunAsync(cancellationTokenSource.Token);
+        using var cts = new CancellationTokenSource();
+        
+        var _ = memoryCleaner.RunAsync(cts.Token);
+        
+        await Task.Delay(1000);
+        cts.Cancel();
     }
 }
